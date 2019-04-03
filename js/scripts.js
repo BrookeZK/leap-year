ae30b56fad536abad1f477e94d7fd22a388a5b7d
Alex Martinez and Brooke Kullberg <brooke@>
2019-04-03 09:57:13 -0700

    add if/else statement to business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
-// User Interface Logic
-$(document).ready(funtion() {
-  $("form#leap-year").submit(function(event) {
-    event.preventDefault();
-    var year = parseInt($("$input#year").val());
-    var result = leapYear(year);
-    $("#result").text(result);
-  });
-});
-
 // Business Logic
 var leapYear = function(year) {
-  return false;
+  if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
+// User Interface Logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+    console.log(result);
+  });
+  console.log(result);
+});
